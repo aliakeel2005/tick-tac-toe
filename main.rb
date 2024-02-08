214ac497abd90c080d221d9b232eeada587f66e2
aliakeel2005 <ali.akeelqq123@gmail.com>
2024-02-08 14:38:41 +0300

    improved code readability

--- main.rb
+++ main.rb
@@ -8,24 +8,27 @@
 
 
 class Board
-  def initialize
-  @tiles = {one: "..", two: "..", three: "..", 
-          four: "..", five: "..", six: "..",
-          seven: "..", eight: "..", nine: ".."}
-  end
-    def display_board
-    puts "|#{@tiles[:one]}|#{@tiles[:two]}|#{@tiles[:three]}|
+def initialize
+  @tiles = {one: 1, two: 2, three: 3, 
+          four: 4, five: 5, six: 6,
+          seven: 7, eight: 8, nine: 9}
+end
+
+def display_board
+puts "|#{@tiles[:one]}|#{@tiles[:two]}|#{@tiles[:three]}|
 |#{@tiles[:four]}|#{@tiles[:five]}|#{@tiles[:six]}|
 |#{@tiles[:seven]}|#{@tiles[:eight]}|#{@tiles[:nine]}|"
-    end
+end
 
-    def add_x(tile)
-        @tiles[tile] = "x"
-        display_board
-    end
-    def add_o(tile)
-        @tiles[tile] = "o"
-    end
+def add_x(tile)
+  @tiles[tile] = "x"
+  display_board
+end
+
+def add_o(tile)
+  @tiles[tile] = "o"
+  display_board
+end
 end
 
 board = Board.new
