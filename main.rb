083f3ec2ec9de1e63b4f691c14e0ea2f422f4266
aliakeel2005 <ali.akeelqq123@gmail.com>
2024-02-08 15:13:52 +0300

    game now asks for user input instead of methods

--- main.rb
+++ main.rb
@@ -1,23 +1,24 @@
 # a class for the board
 # create variable for each tile
-# players add char by methods
+# loop over each players turn until match end
 # each time player add char
 # put ASCII representation of board
 # if row/column full of one char
 # end the game
 
+match_end = false
 
 class Board
 def initialize
-  @tiles = {one: 1, two: 2, three: 3, 
-          four: 4, five: 5, six: 6,
-          seven: 7, eight: 8, nine: 9}
+  @tiles = {1 => 1, 2 => 2, 3 => 3, 
+          4 => 4, 5 => 5, 6 => 6,
+          7 => 7, 8 => 8, 9 => 9}
 end
 
 def display_board
-puts "|#{@tiles[:one]}|#{@tiles[:two]}|#{@tiles[:three]}|
-|#{@tiles[:four]}|#{@tiles[:five]}|#{@tiles[:six]}|
-|#{@tiles[:seven]}|#{@tiles[:eight]}|#{@tiles[:nine]}|"
+puts "|#{@tiles[1]}|#{@tiles[2]}|#{@tiles[3]}|
+|#{@tiles[4]}|#{@tiles[5]}|#{@tiles[6]}|
+|#{@tiles[7]}|#{@tiles[8]}|#{@tiles[9]}|"
 end
 
 def add_x(tile)
@@ -31,5 +32,12 @@ def add_o(tile)
 end
 end
 
+
 board = Board.new
-board.add_x(:one)
+
+while match_end == false
+    puts "P1 turn"
+    board.add_x(gets.chomp.to_i)
+    puts "P2 turn"
+    board.add_o(gets.chomp.to_i)
+end
